4406df7e37a0f84e4d584e7472f44d6f475929c1
LimeTheNigga <limethecoder@gmail.com>
2015-03-04 17:53:21 +0200

    003
    Largest prime factor

---
+++ 003.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+bool isPrime(int);
+
+int main(){
+	long long int num = 600851475143;
+	const int LIMIT = sqrt((double)num);
+	int max = 1, i = 3;
+	
+	do{
+		if (num % i == 0 && isPrime(i))
+			max = i;
+		i++;
+	} while (i <= LIMIT);
+
+	printf("%d\n", max);
+	return 0;
+}
+
+bool isPrime(int n) {
+	int sqr = 0;
+
+	if (n % 2 == 0) 
+		return false;
+
+	sqr = (int)sqrt((double)n);
+	
+	for (int i = 3; i <= sqr; i++) {
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
